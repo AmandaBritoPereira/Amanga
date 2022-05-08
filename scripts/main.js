e4fdc5d889cb87a9e25c676599aa37b312a6e4da
Amanda Brito <amandabritopereira@outlook.com>
2022-05-08 19:30:07 -0300

    Acrescentado documentação explicativa

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ var cabecalho_Menu = document.querySelector("#cabecalhoMenu");
 
 button_Expande.addEventListener('click', function (event) {
 
+//addEventListener = Adiciona o evento "escutador", ele mantém a verificação do evento a qualquer momento, e não só no carregamento inicial da página
 
   if (cabecalho_Menu.classList.contains('apareceMenuExpandido')) {
     cabecalho_Menu.classList.remove('apareceMenuExpandido');
